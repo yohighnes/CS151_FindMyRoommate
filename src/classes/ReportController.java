08cf5312d4f093d801bf6239ecadcdc75519a027
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-04-06 00:38:42 -0700

    Add files via upload

---
+++ src/classes/ReportController.java
@@ -0,0 +1,16 @@
+package findMyRoomate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportController {
+	List<Report> reports = new ArrayList<>();
+	
+	public void submit() {
+		return;
+	}
+	
+	public void add() {
+		return;
+	}
+}
