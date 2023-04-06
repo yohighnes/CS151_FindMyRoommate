f170e348170e2dc7d5e52fc1663ffdf94390b01e
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-04-06 00:55:16 -0700

    Add files via upload

---
+++ src/classes/FeedbackController.java
@@ -0,0 +1,16 @@
+package findMyRoomate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedbackController implements SupportService {
+	List<Report> feedbacks = new ArrayList<>();
+	
+	public void submit() {
+		return;
+	}
+	
+	public void add() {
+		return;
+	}
+}
