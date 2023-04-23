e25c4a4670464b1868fec77b028d36c74fa6305c
yohannes Sa habtemariam <98793540+yohighnes@users.noreply.github.com>
2023-04-22 22:45:49 -0700

    Revert "database config and add user info to database"

--- backend/src/main/java/com/example/backend/service/IUserService.java
+++ backend/src/main/java/com/example/backend/service/IUserService.java
@@ -2,7 +2,6 @@ package com.example.backend.service;
 
 import com.example.backend.model.User;
 
-
 public interface IUserService {
     User registerUser(String firstName, String lastName, String email, String password, String username);
     boolean isValidPassword(String password);
