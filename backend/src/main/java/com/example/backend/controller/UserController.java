032b1ca68f789f1cdf4a649736d1bdfa326cbaaa
yohannes Sa habtemariam <yohannessahle100@gmail.com>
2023-04-23 00:30:19 -0700

    db and signup

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,30 +1,42 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.User;
+import com.example.backend.repository.UserRepository;
 import com.example.backend.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/user")
 public class UserController {
 
     @Autowired
-    private IUserService userService;
+    private UserRepository userRepository;
 
-    @GetMapping("/register")
-    public String registerUser(@RequestParam(name="firstName", defaultValue = "") String firstName,
-                               @RequestParam(name="lastName", defaultValue = "") String lastName,
-                               @RequestParam(name="usernamd", defaultValue = "") String username,
-                               @RequestParam(name="password", defaultValue = "") String password,
-                               @RequestParam(name="email", defaultValue = "") String email) {
-        User user = userService.registerUser(firstName, lastName, email, password, username);
+    @PostMapping(path = "/add")
+    public @ResponseBody User addNewUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password) {
 
-        if(user == null) {
-
-        }
-        return "Registered";
+        User user = new User();
+        user.setName(firstName + " " + lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+        return userRepository.save(user);
     }
+    
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable < User > getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
+
+
 
--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.example.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    // methods to perform CRUD operations on the database
 }
+
+
