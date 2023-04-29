a8a648449bfc9979a959dc2a4d0bb77ac34beda8
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-04-29 14:39:22 -0700

    Update ContactController.java

--- backend/src/main/java/com/example/backend/controller/ContactController.java
+++ backend/src/main/java/com/example/backend/controller/ContactController.java
@@ -1,5 +1,10 @@
 package com.example.backend.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,32 +15,30 @@ public class ContactController {
     @Autowired
     private JavaMailSender mailSender;
 
-
     @GetMapping("/contactUs")
     public String showContactForm() {
         return "contact_form";
     }
-    
+
     @PostMapping("/contactUs")
     public String submitContact(HttpServletRequest request) {
         String fullname = request.getParameter("fullname");
         String email = request.getParameter("email");
-        String message = request.getParameter("message");
+        String messageText = request.getParameter("message");
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("hoang.khanhnguyen2004@gmail.com") //using a sender gmail to test
-        message.setTo("enteryourowngmail.com"); //this is where we suppose to get the gmail
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("hoang.khanhnguyen2004@gmail.com");
+        mailMessage.setTo("enteryourowngmail.com");
 
-
-        String mailSubject = fullname + "has sent a message";
-        String mailContent = "Sender Name" + fullname + "\n";
+        String mailSubject = fullname + " has sent a message";
+        String mailContent = "Sender Name: " + fullname + "\n";
         mailContent += "Sender E-mail: " + email + "\n";
-        mailContent += "Message: " + message + "\n";
+        mailContent += "Message: " + messageText + "\n";
 
-        message.setSubject(mailSubject)
-        message.setText(mailContent);
+        mailMessage.setSubject(mailSubject);
+        mailMessage.setText(mailContent);
 
-        mailSender.send(message);
+        mailSender.send(mailMessage);
         return "message";
     }
 }
