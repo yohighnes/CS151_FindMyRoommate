6219f1db0878b1e20be8cc3439c90136d48e6c0e
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-05-05 00:31:56 -0700

    Update ContactUsForm.js

--- frontend/src/components/contactUsForm/ContactUsForm.js
+++ frontend/src/components/contactUsForm/ContactUsForm.js
@@ -6,23 +6,46 @@ import { useState } from 'react';
 function ContactUsForm() {
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
+    const [text, setText] = useState("")
 
-    const handleEmail = (e) => {
-        e.preventDefault();
-        setEmail(e.target.value);
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
     }
 
-    const handleName = (e) => {
-        e.preventDefault();
-        setName(e.target.value);
-    }
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleMessageChange = (event) => {
+        setText(event.target.value);
+    };
 
     const handleOnSubmit = (e) => {
-        e.preventDefault();
-        setEmail('');
-        setName('');
+        const emailInfo = {
+            email: email,
+            name: name,
+            text: text
+        };
 
+          // send email information to backend
+        fetch('http://localhost:8080/contactUs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(emailInfo)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to send email');
+            }
+            console.log('Email sent successfully');
+        })
+        .catch(error => {
+            console.error(error);
+        });
     }
+
     return (
         <div className="contactus-container">
             <div className="contactus-box">
@@ -35,13 +58,13 @@ function ContactUsForm() {
                 </div>
                 <Form>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Control type="textarea" placeholder="Name" onChange={handleName} value={name} />
+                        <Form.Control type="textarea" placeholder="Name" onChange={handleNameChange} value={name} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Control type="email" placeholder="Email" onChange={handleEmail} value={email} />
+                        <Form.Control type="email" placeholder="Email" onChange={handleEmailChange} value={email} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Control placeholder="Message" as="textarea" rows={5} />
+                        <Form.Control type="textarea" placeholder="Message" onChange={handleMessageChange} value={text} as="textarea" rows={5} />
                     </Form.Group>
                     <button className="btn sendBtn" id="submit" type="submit" value="SEND" style={{ backgroundColor: 'orange' }} onClick={handleOnSubmit}>
                         <div className="alt-send-button">
